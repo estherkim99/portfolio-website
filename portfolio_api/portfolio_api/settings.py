8564df1ebc734b369e7f0d1f507c45774381c3ec
Esther Kim <estherkim1721@gmail.com>
2020-09-28 20:53:42 +0900

    fix: resolve elastic beanstalk deployment errors

--- portfolio_api/portfolio_api/settings.py
+++ portfolio_api/portfolio_api/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '.elasticbeanstalk.com',
+    'localhost'
+]
 
 
 # Application definition
